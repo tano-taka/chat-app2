3a16e909bb907190e391081dbbab282d55dd4117
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-04 21:44:31 +0900

    メッセージ送信機能の実装

--- db/migrate/20210304113452_create_messages.rb
+++ db/migrate/20210304113452_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.string :content
+      t.string  :content
       t.references :user, foreign_key: true
-      t.regerences :room, foreign_key: true
+      t.references :room, foreign_key: true
       t.timestamps
     end
   end
